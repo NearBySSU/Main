c2b4acc6b997573a76327b14763b27dcb4298758
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-10-28 00:19:40 +0900

    회원 가입 로그인 구현 완료

---
+++ app/src/main/java/com/example/nearby/SignUpActivity.java
@@ -0,0 +1,54 @@
+package com.example.nearby;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.nearby.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(view);
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = binding.emailText.getText().toString().trim();
+                final String pwd = binding.passwordText.getText().toString().trim();
+
+                mAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(SignUpActivity.this, MainPageActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
