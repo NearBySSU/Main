441e52677cb245c43691a67037780c2f771911f0
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-10-27 20:11:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nearby/LoadingActivity.java
@@ -0,0 +1,29 @@
+package com.example.nearby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        //로딩 시작
+        loadingStart();
+    }
+
+    private void loadingStart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(), MainPageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
