ff061633c4d08620e545712a9486e5afdd150c9d
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-10-29 00:05:00 +0900

    위치권한 요청,지도를 현재 위치로 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,5 +59,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("androidx.navigation:navigation-fragment:2.7.4")
     implementation ("androidx.navigation:navigation-ui:2.7.4")
+    implementation(platform("com.google.firebase:firebase-bom:32.4.1"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation ("com.google.android.gms:play-services-location:18.0.0")
 
 }
