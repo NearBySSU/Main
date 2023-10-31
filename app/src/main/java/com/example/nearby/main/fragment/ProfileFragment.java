4674372c81b615cf319afd80759357026f1455d4
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-10-31 11:36:40 +0900

    로그아웃 버튼

--- app/src/main/java/com/example/nearby/main/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/nearby/main/fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.nearby.main.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.nearby.R;
+import com.example.nearby.auth.LogInActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class ProfileFragment extends Fragment {
+
+    private Button logoutButton;
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        logoutButton = view.findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(getActivity(), LogInActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        return view;
     }
 }
