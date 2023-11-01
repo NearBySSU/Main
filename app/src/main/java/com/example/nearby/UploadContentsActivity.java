6ed5aed025f108ec6698422a911907b70facb61f
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-11-01 13:47:35 +0900

    게시물 업로드시 날짜 선택

--- app/src/main/java/com/example/nearby/UploadContentsActivity.java
+++ app/src/main/java/com/example/nearby/UploadContentsActivity.java
@@ -4,13 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -22,6 +25,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -36,8 +40,10 @@ public class UploadContentsActivity extends AppCompatActivity {
     StorageReference storageRef = storage.getReference();
 
     EditText editText;
-    Button uploadButton;
+    Button uploadButton, pickDateButton;
+    TextView showDateTextView;
     Uri imageUri;
+    String selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +52,9 @@ public class UploadContentsActivity extends AppCompatActivity {
 
         editText = findViewById(R.id.mainText);
         uploadButton = findViewById(R.id.upload_button);
+        pickDateButton = findViewById(R.id.pick_date_button);
+        showDateTextView = findViewById(R.id.show_date_textView);
+
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,9 +63,39 @@ public class UploadContentsActivity extends AppCompatActivity {
                 }
             }
         });
+
+        pickDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickDate();
+            }
+        });
     }
 
-    //이미지를 갤러리에서 가져오는 함수
+
+
+
+
+
+
+    /*------------------------------------------------------------------------------날짜 선택 함수-------------------------------------------------------------------------------------*/
+    public void pickDate() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(UploadContentsActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                selectedDate = year+"년" + dayOfMonth + "월" + (month + 1) + "일" ;
+                showDateTextView.setText("Selected date: " + selectedDate);
+            }
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    /*------------------------------------------------------------------------------이미지를 갤러리에서 가져오는 함수-------------------------------------------------------------------------------------*/
     public void pickImageFromGallery(View view) {
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -73,7 +112,7 @@ public class UploadContentsActivity extends AppCompatActivity {
         }
     }
 
-    //포스트를 업로드하는 함수
+    /*------------------------------------------------------------------------------포스트 업로드 함수-------------------------------------------------------------------------------------*/
     private void uploadPost(String text, Uri imageUri) {
         StorageReference imageRef = storageRef.child("images/" + UUID.randomUUID().toString());
         imageRef.putFile(imageUri)
@@ -86,6 +125,7 @@ public class UploadContentsActivity extends AppCompatActivity {
                                 Map<String, Object> post = new HashMap<>();
                                 post.put("text", text);
                                 post.put("imageUrl", uri.toString());
+                                post.put("date", selectedDate); // 선택된 날짜 업로드
 
                                 db.collection("posts")
                                         .add(post)
@@ -117,4 +157,5 @@ public class UploadContentsActivity extends AppCompatActivity {
                     }
                 });
     }
+
 }
