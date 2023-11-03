748d9fb2396700d107ff1937b5ddddce10a95d85
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-11-03 09:13:00 +0900

    [ADD]지도에 게시물 마커 띄우기

--- app/src/main/java/com/example/nearby/main/fragment/MapsFragment.java
+++ app/src/main/java/com/example/nearby/main/fragment/MapsFragment.java
@@ -4,13 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.example.nearby.Post;
+import com.example.nearby.PostAdapter;
 import com.example.nearby.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,11 +25,26 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MapsFragment extends Fragment {
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap mMap;
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+
+    private List<Post> postList;
+    //포스트를 위한 어댑터
+    private PostAdapter postAdapter;
+    //기준 거리
+    public float pivot_meter = 1000;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
@@ -60,6 +81,12 @@ public class MapsFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        db = FirebaseFirestore.getInstance();
+        postList = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recyclerView);
+        postAdapter = new PostAdapter(postList);
+        loadNearbyPosts();
     }
 
     //위치 권한 요청
@@ -82,6 +109,52 @@ public class MapsFragment extends Fragment {
             if (location != null) {
                 LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+            }
+        });
+    }
+
+    //근처 포스트를 로드하는 함수
+    public void loadNearbyPosts() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getActivity(),"포스트 로드 실패! 위치권한을 허용해 주세요",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+            if (location != null) {
+                getPosts(location);
+            }
+        });
+    }
+
+    //현재 위치와 거리를 재서, 일정 위치 안에 있는 포스트만 postList에 추가
+    public void getPosts(Location currentLocation) {
+        db.collection("posts").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                    String text = document.getString("text");
+                    double latitude = document.getDouble("latitude");
+                    double longitude = document.getDouble("longitude");
+
+                    Location postLocation = new Location("");
+                    postLocation.setLatitude(latitude);
+                    postLocation.setLongitude(longitude);
+
+                    //거리 재기
+                    float distanceInMeters = currentLocation.distanceTo(postLocation);
+
+                    //거리 비교해서 list에 넣기
+                    if (distanceInMeters < pivot_meter) {
+                        Post post = new Post(document.getId(),text, latitude, longitude);
+                        postList.add(post);
+
+                        // 게시물의 위치에 마커 추가
+                        LatLng postLatLng = new LatLng(latitude, longitude);
+                        mMap.addMarker(new MarkerOptions().position(postLatLng).title(text));
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
             }
         });
     }
