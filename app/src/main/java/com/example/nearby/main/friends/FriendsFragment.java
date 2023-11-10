68042b4202fdfc39454118f86e763c0883e1ed78
OhDG <ohdonggyu@naver.com>
2023-11-10 13:53:13 +0900

    [FEAT] Following 버튼 구현 완료

--- app/src/main/java/com/example/nearby/main/friends/FriendsFragment.java
+++ app/src/main/java/com/example/nearby/main/friends/FriendsFragment.java
@@ -22,26 +22,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 
+import java.util.List;
+
 public class FriendsFragment extends Fragment {
-
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_friends, container, false);
-//    }
-
     private EditText findEmailEdit;
     private Button followBtn;
     private Button unfollowBtn;
 
     private FirebaseAuth auth;
-    private FirebaseStorage storage;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private DatabaseReference ref;
@@ -65,10 +60,7 @@ public class FriendsFragment extends Fragment {
         followBtn = view.findViewById(R.id.followBtn);
         unfollowBtn = view.findViewById(R.id.unFollowBtn);
 
-        String currentUserId = auth.getCurrentUser().getUid();
-//        DocumentReference txDocTargetUser = firestore.collection("users").document(currentUserId);
-
-        Log.d("ODG", currentUserId);
+        Log.d("ODG", currentUid);
 
         followBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,12 +78,45 @@ public class FriendsFragment extends Fragment {
                                     if (task.getResult().isEmpty()) {
                                         // 일치하는 이메일이 없는 경우
                                         Log.d("ODG", "No matching email found.");
+                                        Toast.makeText(getContext(), "존재하지 않는 friend입니다.", Toast.LENGTH_SHORT).show();
                                     } else {
                                         // 일치하는 이메일이 있는 경우
                                         for (QueryDocumentSnapshot document : task.getResult()) {
                                             String findUid = document.getId();
-                                            onFollowingAdded(findUid);
-                                            Log.d("ODG", document.getId() + " => " + document.getData());
+
+                                            // 자신 추가가 아니라면
+                                            if ( !findUid.equals(currentUid) ) {
+
+                                                // 이미 followings db에 존재하는지 검사
+                                                DocumentReference docRef = db.collection("users").document(currentUid);
+                                                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                        if (task.isSuccessful()) {
+                                                            DocumentSnapshot document = task.getResult();
+                                                            if (document.exists()) {
+                                                                List<String> followings = (List<String>) document.get("followings");
+                                                                if (followings.contains(findUid)) {
+                                                                    Log.d("ODG", "입력값이 배열에 존재합니다.");
+                                                                    Toast.makeText(getContext(), "You can't follow same account twice", Toast.LENGTH_SHORT).show();
+                                                                } else {
+                                                                    Log.d(TAG, "입력값이 배열에 존재하지 않습니다. 친구추가할게요");
+                                                                    onFollowingAdded(findUid);
+                                                                    Log.d("ODG", document.getId() + " => " + document.getData());
+                                                                    Toast.makeText(getContext(), "perfect friend add", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            } else {
+                                                                Log.d(TAG, "No such document");
+                                                            }
+                                                        } else {
+                                                            Log.d(TAG, "get failed with ", task.getException());
+                                                        }
+                                                    }
+                                                });
+                                            } else {
+                                                // 자추 는 안돼요
+                                                Toast.makeText(getContext(), "You can't follow your account by self!", Toast.LENGTH_SHORT).show();
+                                            }
                                         }
                                     }
                                 } else {
@@ -99,29 +124,9 @@ public class FriendsFragment extends Fragment {
                                 }
                             }
                         });
-
-
-
             }
         });
-
-
-
-
-
-//    public class FollowDto {
-//        // 이 사람을 팔로잉하는 사람들
-//        private Map<String, Boolean> followers = new HashMap<>();
-//
-//        // 이 사람이 팔로잉 중인 사람들
-//        private Map<String, Boolean> followings = new HashMap<>();
-//
-//        // getters and setters
-
-//    }
-
         return view;
-
     }
 
 
@@ -133,7 +138,7 @@ public class FriendsFragment extends Fragment {
         DocumentReference userRef = db.collection("users").document(currentUid);
         userRef.update("followings", FieldValue.arrayUnion(inputUid))
                 .addOnSuccessListener(aVoid -> Log.d("ODG", "InputId added to user followings document"))
-                .addOnFailureListener(e -> Log.w("ODG", "Error adding userID to user followings documnet", e));
+                .addOnFailureListener(e -> Log.w("ODG", "Error adding userID to user followings document", e));
 
         Log.d("ODG", "Following added with ID: " + inputUid);
         Toast.makeText(getContext(), "팔로잉 성공!", Toast.LENGTH_SHORT).show();
