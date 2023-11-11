a619023b7f66136195cc97d33f89cbe545248184
OhDG <ohdonggyu@naver.com>
2023-11-12 01:18:42 +0900

    [FEAT] unfollow 버튼 구현 완료

--- app/src/main/java/com/example/nearby/main/friends/FriendsFragment.java
+++ app/src/main/java/com/example/nearby/main/friends/FriendsFragment.java
@@ -62,6 +62,7 @@ public class FriendsFragment extends Fragment {
 
         Log.d("ODG", currentUid);
 
+        // 팔로우 버튼
         followBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,7 +79,7 @@ public class FriendsFragment extends Fragment {
                                     if (task.getResult().isEmpty()) {
                                         // 일치하는 이메일이 없는 경우
                                         Log.d("ODG", "No matching email found.");
-                                        Toast.makeText(getContext(), "존재하지 않는 friend입니다.", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getContext(), "이 친구는 우리 앱에 없어요. 친구에게 추천해주세요.", Toast.LENGTH_SHORT).show();
                                     } else {
                                         // 일치하는 이메일이 있는 경우
                                         for (QueryDocumentSnapshot document : task.getResult()) {
@@ -98,12 +99,12 @@ public class FriendsFragment extends Fragment {
                                                                 List<String> followings = (List<String>) document.get("followings");
                                                                 if (followings.contains(findUid)) {
                                                                     Log.d("ODG", "입력값이 배열에 존재합니다.");
-                                                                    Toast.makeText(getContext(), "You can't follow same account twice", Toast.LENGTH_SHORT).show();
+                                                                    Toast.makeText(getContext(), "이 친구는 이미 팔로우 되어 있어요.", Toast.LENGTH_SHORT).show();
                                                                 } else {
                                                                     Log.d(TAG, "입력값이 배열에 존재하지 않습니다. 친구추가할게요");
                                                                     onFollowingAdded(findUid);
                                                                     Log.d("ODG", document.getId() + " => " + document.getData());
-                                                                    Toast.makeText(getContext(), "perfect friend add", Toast.LENGTH_SHORT).show();
+                                                                    Toast.makeText(getContext(), "지금부터 이 친구를 팔로잉해요.", Toast.LENGTH_SHORT).show();
                                                                 }
                                                             } else {
                                                                 Log.d(TAG, "No such document");
@@ -115,7 +116,75 @@ public class FriendsFragment extends Fragment {
                                                 });
                                             } else {
                                                 // 자추 는 안돼요
-                                                Toast.makeText(getContext(), "You can't follow your account by self!", Toast.LENGTH_SHORT).show();
+                                                Toast.makeText(getContext(), "나 자신을 아무리 사랑해도 나를 팔로우할 순 없어요.", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    Log.d("ODG", "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+            }
+        });
+
+
+
+    // 언팔로우 버튼
+        unfollowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputEmail = findEmailEdit.getText().toString();
+                Log.d("ODG", inputEmail);
+
+                db.collection("users")
+                        .whereEqualTo("email", inputEmail)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult().isEmpty()) {
+                                        // 일치하는 이메일이 없는 경우
+                                        Log.d("ODG", "No matching email found.");
+                                        Toast.makeText(getContext(), "이 친구는 우리 앱에 없어요. 친구에게 추천해주세요.", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // 일치하는 이메일이 있는 경우
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            String findUid = document.getId();
+
+                                            // 자신 추가가 아니라면
+                                            if ( !findUid.equals(currentUid) ) {
+
+                                                // 이미 followings db에 존재하는지 검사
+                                                DocumentReference docRef = db.collection("users").document(currentUid);
+                                                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                        if (task.isSuccessful()) {
+                                                            DocumentSnapshot document = task.getResult();
+                                                            if (document.exists()) {
+                                                                List<String> followings = (List<String>) document.get("followings");
+                                                                if (followings.contains(findUid)) {
+                                                                    Log.d("ODG", "입력값이 배열에 존재합니다. 고로 삭제합니다.");
+                                                                    onFollowingRemoved(findUid);
+                                                                    Toast.makeText(getContext(), "지금부터 이 친구를 팔로잉하지 않아요.", Toast.LENGTH_SHORT).show();
+                                                                } else {
+                                                                    Log.d(TAG, "입력값이 배열에 존재하지 않아요.");
+                                                                    Log.d("ODG", document.getId() + " => " + document.getData());
+                                                                    Toast.makeText(getContext(), "이 친구는 이미 팔로우 되어 있지 않아요.", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            } else {
+                                                                Log.d(TAG, "No such document");
+                                                            }
+                                                        } else {
+                                                            Log.d(TAG, "get failed with ", task.getException());
+                                                        }
+                                                    }
+                                                });
+                                            } else {
+                                                // 자삭 은 안돼요
+                                                Toast.makeText(getContext(), "나를 팔로우 취소할 순 없어요. 나 자신을 사랑해주세요.", Toast.LENGTH_SHORT).show();
                                             }
                                         }
                                     }
@@ -142,7 +211,17 @@ public class FriendsFragment extends Fragment {
 
         Log.d("ODG", "Following added with ID: " + inputUid);
         Toast.makeText(getContext(), "팔로잉 성공!", Toast.LENGTH_SHORT).show();
+    }
 
+    private void onFollowingRemoved(String inputUid) {
+        // 검색된 이메일의 사용자 id를 following DB에서 삭제
 
+        DocumentReference userRef = db.collection("users").document(currentUid);
+        userRef.update("followings", FieldValue.arrayRemove(inputUid))
+                .addOnSuccessListener(aVoid -> Log.d("ODG", "InputId removed to user followings document"))
+                .addOnFailureListener(e -> Log.w("ODG", "Error removing userID to user followings document", e));
+
+        Log.d("ODG", "Following removed with ID: " + inputUid);
+        Toast.makeText(getContext(), "언팔로잉 성공!", Toast.LENGTH_SHORT).show();
     }
 }
