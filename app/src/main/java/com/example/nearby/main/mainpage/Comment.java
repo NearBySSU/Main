1f62ead91ecd1289c3d0ebbf9a348d5eb1f94b82
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-11-11 22:35:55 +0900

    [EDIT]댓글창 수정

--- app/src/main/java/com/example/nearby/main/mainpage/Comment.java
+++ app/src/main/java/com/example/nearby/main/mainpage/Comment.java
@@ -1,13 +1,19 @@
 package com.example.nearby.main.mainpage;
 
 public class Comment {
-    private String commentText;
-    private String commenterId;
+    private String commentText; // 댓글 내용
+    private String commenterId; // 댓글 작성자 ID
+    private long timestamp; // 댓글 작성 시간
+    private String profilePicUrl; // 댓글 작성자의 프로필 사진 URL
+
 
     // 생성자
-    public Comment(String commentText, String commenterId) {
+    public Comment(String commentText, String commenterId, String profilePicUrl, long timestamp) {
         this.commentText = commentText;
         this.commenterId = commenterId;
+        this.profilePicUrl = profilePicUrl;
+        this.timestamp = timestamp;
+
     }
 
     // getter 메소드
@@ -18,4 +24,6 @@ public class Comment {
     public String getCommenterId() {
         return this.commenterId;
     }
+    public long getTimestamp(){return this.timestamp;}
+    public String getProfilePicUrl(){return  this.profilePicUrl;}
 }
--- app/src/main/java/com/example/nearby/main/mainpage/CommentAdapter.java
+++ app/src/main/java/com/example/nearby/main/mainpage/CommentAdapter.java
@@ -3,13 +3,19 @@ package com.example.nearby.main.mainpage;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.nearby.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
 
@@ -31,7 +37,10 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
         Comment comment = commentList.get(position);
         holder.tvCommentText.setText(comment.getCommentText());
-        // 필요에 따라 다른 댓글 정보를 설정할 수 있습니다.
+        holder.tvTimestamp.setText(getFormattedTimestamp(comment.getTimestamp())); // 시간 정보 설정
+        Glide.with(holder.ivProfilePic.getContext())
+                .load(comment.getProfilePicUrl())
+                .into(holder.ivProfilePic); // 프로필 사진 설정
     }
 
     @Override
@@ -41,13 +50,24 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
 
     public static class CommentViewHolder extends RecyclerView.ViewHolder {
         TextView tvCommentText; // 댓글 내용 텍스트뷰
+        TextView tvTimestamp; // 댓글 작성 시간 텍스트뷰
+        ImageView ivProfilePic; // 댓글 작성자의 프로필 사진 이미지뷰
+
 
         public CommentViewHolder(@NonNull View itemView) {
             super(itemView);
             tvCommentText = itemView.findViewById(R.id.tvCommentText);
+            tvTimestamp = itemView.findViewById(R.id.tvTimestamp); // 뷰 바인딩
+            ivProfilePic = itemView.findViewById(R.id.ivProfilePic); // 뷰 바인딩
         }
     }
 
-
+    // 시간 정보를 보기 좋게 변환하는 메소드
+    private String getFormattedTimestamp(long timestamp) {
+        Date date = new Date(timestamp);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+        return formatter.format(date);
+    }
 }
+
 
