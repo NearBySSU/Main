964783c0a8c228bb7704ca54de3c3160910b9410
Lee-min-gue <125437775+Lee-min-gue@users.noreply.github.com>
2023-11-21 17:11:17 +0900

    [ADD]post_item에 버튼 추가

--- app/src/main/java/com/example/nearby/main/upload/UploadContentsActivity.java
+++ app/src/main/java/com/example/nearby/main/upload/UploadContentsActivity.java
@@ -57,7 +57,6 @@ public class UploadContentsActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
     private static final String TAG = "UploadContentsActivity";
     ArrayList<Uri> uriList = new ArrayList<>();     // 이미지의 uri를 담을 ArrayList 객체
-
     ArrayList<String> checkedTags = new ArrayList<>();    // 체크된 Chip들의 ID를 저장할 ArrayList 생성
 
     RecyclerView recyclerView;  // 이미지를 보여줄 리사이클러뷰
