aa3051e6d303994d2ad3b8f19dc19f7b95958d7e
OhDG <ohdonggyu@naver.com>
2023-11-29 16:58:13 +0900

    [FEAT] SinglePostPage 연결 후 db 데이터 가져오기

--- app/src/main/java/com/example/nearby/main/SinglePostPageActivity.java
+++ app/src/main/java/com/example/nearby/main/SinglePostPageActivity.java
@@ -2,69 +2,144 @@ package com.example.nearby.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.bumptech.glide.Glide;
+import com.example.nearby.R;
 import com.example.nearby.databinding.ActivityLogInBinding;
 import com.example.nearby.databinding.ActivitySinglePostPageBinding;
+import com.example.nearby.main.mainpage.CommentBottomSheetDialogFragment;
+import com.example.nearby.main.mainpage.ImageAdapter;
 import com.example.nearby.main.mainpage.Post;
 import com.example.nearby.main.mainpage.PostAdapter;
+import com.example.nearby.main.mainpage.TagsAdapter;
+import com.example.nearby.main.maps.MapsFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class SinglePostPageActivity extends AppCompatActivity {
     private FirebaseFirestore db;
-    private RecyclerView recyclerView;
-    private PostAdapter postAdapter;
+    private FirebaseAuth auth;
+    private String profileImageUrl; // userid로 찾아줌
+    private String nickName; // userid로 찾아줌
+    private ImageButton mapImageButton; // 걍 버튼
+    private RecyclerView imageRecyclerView; // 배열일단 받고
+    private String date;
+    private String bigLocation;
+    private String smallLocation;
+    private RecyclerView tagRecyclerView; // 배열일단받고
+    private String text;
+    private ImageButton likeBtnImageButton; //  아님
+    private ImageButton commentBtnImageButton; // 아님
+    private ImageAdapter imageAdapter;
+    private TagsAdapter tagsAdapter;
+
     private ActivitySinglePostPageBinding binding;
-//    private PostLoader postLoader;
-    private List<Post> postList = new ArrayList<>();
+    private String postId;
+    private String postUid;
+    private String uid;
+    private List<String> imageUrls = new ArrayList<>();
+    private List<String> likeList = new ArrayList<>();
+    private List<String> tags = new ArrayList<>();
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySinglePostPageBinding.inflate(getLayoutInflater());
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        uid = auth.getUid();
         Intent intent = getIntent();
-        String postId = intent.getStringExtra("postId");
+        postId = intent.getStringExtra("postId");
 
         Log.d("singlepage", postId);
-//        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//        // recyclerView 등록
-//        postAdapter = new PostAdapter(postList);
-//        binding.recyclerView.setAdapter(postAdapter);
-//
-//        // 스와이프 이벤트
+
+        // 스와이프 이벤트
 //        binding.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
 //            @Override
 //            public void onRefresh() {
-//                postList.clear();
-//                postAdapter.setPostList(postList);
-//                getPost(postId);
+//                imageUrls.clear();
+//                tags.clear();
+//                likeList.clear();
+//                imageAdapter.notifyDataSetChanged();
+//                tagsAdapter.notifyDataSetChanged();
+//                getPostData(postId);
+//                getUserData(postUid);
+//                registerPostData();
 //                binding.swipeRefreshLayout.setRefreshing(false);
 //            }
 //        });
+
+        // post 로드 하기
+
+        imageUrls.clear();
+        tags.clear();
+        likeList.clear();
+//        imageAdapter.notifyDataSetChanged();
+//        tagsAdapter.notifyDataSetChanged();
+        getPostData(postId);
+//        registerPostData();
+
+
+
+
+        // button onclick 선언
+//        binding.icReply.setOnClickListener(v -> {
+//            CommentBottomSheetDialogFragment fragment =
+//                    CommentBottomSheetDialogFragment.newInstance(postId);
+//            fragment.show(((FragmentActivity) v.getContext()).getSupportFragmentManager(), "commentDialog");
+//        });
 //
-//        // post list 로드 하기
-//        postList.clear();
-//        postAdapter.notifyDataSetChanged();
+//        binding.icEmptyHeart.setOnClickListener(v -> {
+//            checkLikeStatus(postId, uid, binding.icEmptyHeart);
+//        });
 //
-//        getPost(postId);
+//        binding.btnMap.setOnClickListener(v -> {
+//            // MapsFragment의 인스턴스를 가져옵니다.
+//            MapsFragment mapsFragment = MapsFragment.getInstance();
 //
-//    }
+//            // 포스트 아이디를 설정합니다.
+//            mapsFragment.setPostId(postId);
 //
-//    private void getPost(String postId) {
+//            // 화면을 MapsFragment로 전환합니다.
+//            ((FragmentActivity) v.getContext()).getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.containers, mapsFragment)
+//                    .addToBackStack(null)
+//                    .commit();
+//
+//            ((MainPageActivity) v.getContext()).nullPostId = false;
+//
+//            BottomNavigationView bottomNavigationView = ((MainPageActivity) v.getContext()).findViewById(R.id.bottom_navigationView);
+//            bottomNavigationView.setSelectedItemId(R.id.MapNav);
+//
+//            ((MainPageActivity) v.getContext()).nullPostId = true;
+//
+//        });
+    }
+
+//    private void getPostData(String postId) {
 //        db.collection("posts").document(postId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
 //            @Override
 //            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -72,25 +147,23 @@ public class SinglePostPageActivity extends AppCompatActivity {
 //                    DocumentSnapshot document = task.getResult();
 //                    if (document.exists()) {
 //
-//                        // 데이터 로드
-//                        double latitude = document.getDouble("latitude");
-//                        double longitude = document.getDouble("longitude");
-//                        String uid = document.getString("uid");
-//                        String date = document.getString("date");
-//                        String bigLocationName = document.getString("bigLocationName");
-//                        String smallLocationName = document.getString("smallLocationName");
-//                        List<String> imageUrls = (List<String>) document.get("imageUrls");
-//                        List<String> likeList = (List<String>) document.get("likes");
-//                        List<String> tags = (List<String>) document.get("tags");
-//                        String text = document.getString("text");
+//                        // 인스턴스 초기화
+//                        postUid = document.getString("uid"); // nickname, profileimage 설정예정
 //
-//                        // Post 객체 생성
-//                        Post post = new Post(document.getId(), text, bigLocationName, smallLocationName, latitude, longitude, date, uid, imageUrls, likeList,tags);
-//                        postList.add(post);
-//                        postAdapter.notifyDataSetChanged();  // 데이터가 추가될 때마다 UI 갱신
+//                        date = document.getString("date");
+//                        bigLocation = document.getString("bigLocationName");
+//                        smallLocation = document.getString("smallLocationName");
+//                        text = document.getString("text");
 //
-//                        Log.d("SinglePost", "DocumentSnapshot data: " + document.getData());
+//                        // recyclerView list 초기화
+//                        imageUrls = (List<String>) document.get("imageUrls");
+//                        likeList = (List<String>) document.get("likes");
+//                        tags = (List<String>) document.get("tags");
 //
+////                        Log.d("SinglePost", "DocumentSnapshot data: " + document.getData());
+//                        Log.d("SinglePage", postUid != null ? postUid : "postUid is null");
+//                        Log.d("SinglePage", date != null ? date : "date is null");
+//                        Log.d("SinglePage", text != null ? text : "text is null");
 //                    } else {
 //                        Log.d("SinglePost", "No such document");
 //                    }
@@ -99,5 +172,179 @@ public class SinglePostPageActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
+//    }
+
+
+
+    // 수정본
+    private void getPostData(String postId) {
+        db.collection("posts").document(postId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+
+                        // 인스턴스 초기화
+                        postUid = document.contains("uid") ? document.getString("uid") : ""; // nickname, profileimage 설정예정
+                        date = document.contains("date") ? document.getString("date") : "";
+                        bigLocation = document.contains("bigLocationName") ? document.getString("bigLocationName") : "";
+                        smallLocation = document.contains("smallLocationName") ? document.getString("smallLocationName") : "";
+                        text = document.contains("text") ? document.getString("text") : "";
+
+                        // recyclerView list 초기화
+                        imageUrls = document.contains("imageUrls") ? (List<String>) document.get("imageUrls") : new ArrayList<>();
+                        likeList = document.contains("likes") ? (List<String>) document.get("likes") : new ArrayList<>();
+                        tags = document.contains("tags") ? (List<String>) document.get("tags") : new ArrayList<>();
+
+                        getUserData(postUid);
+
+                        Log.d("SinglePage", postUid != null ? postUid : "postUid is null");
+                        Log.d("SinglePage", date != null ? date : "date is null");
+                        Log.d("SinglePage", text != null ? text : "text is null");
+
+                        Log.d("SinglePost", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d("SinglePost", "No such document");
+                    }
+                } else {
+                    Log.d("SinglePost", "get failed with ", task.getException());
+                }
+            }
+        });
     }
+
+
+
+
+
+    private void getUserData(String postUid) {
+        db.collection("users").document(postUid)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document != null) {
+                            profileImageUrl = document.getString("profilePicUrl");
+                            nickName = document.getString("nickname");
+
+                            Log.d("SinglePage", profileImageUrl != null ? profileImageUrl : "text is null");
+                            Log.d("SinglePage", nickName != null ? nickName : "text is null");
+                        }
+                    }
+                    else {
+                        Log.d("SinglePost", "get failed with ", task.getException());
+                    }
+                });
+    }
+//
+//    private void registerPostData() {
+//        // binding
+//
+//        binding.tvPostDate.setText(date);
+//        binding.tvBigLocationName.setText(bigLocation);
+//        binding.tvSmallLocationName.setText(smallLocation);
+//        binding.tvPostMainText.setText(text);
+//
+//        Glide.with(binding.imgProfile.getContext())
+//                .load(profileImageUrl)
+//                .circleCrop()
+//                .error(R.drawable.stock_profile)
+//                .into(binding.imgProfile);
+//
+//        binding.tvNickName.setText(nickName);
+//
+//
+//        // Image Post recyclerView 등록
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+//        binding.imgPostRecyclerView.setLayoutManager(layoutManager);
+//        imageAdapter = new ImageAdapter(this, imageUrls);
+//        binding.imgPostRecyclerView.setAdapter(imageAdapter);
+//
+//        // Tag recyclerView 등록
+//        LinearLayoutManager tagsLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true);
+//        layoutManager.setReverseLayout(true);
+//        layoutManager.setStackFromEnd(true);
+//        binding.tagRecyclerview.setLayoutManager(tagsLayoutManager);
+//        List<String> uniqueTags = new ArrayList<>(new LinkedHashSet<>(tags));
+//        tagsAdapter = new TagsAdapter(uniqueTags);
+//        binding.tagRecyclerview.setAdapter(tagsAdapter);
+//
+//        // 좋아요 상태 불러오기
+//        //        String uid = auth.getUid();
+//        //        List<String> likes = post.getLikes();
+//        if (likeList != null && likeList.contains(uid)) {
+//            // 좋아요를 이미 눌렀으면 꽉 찬 하트 이미지 로드
+//            binding.icEmptyHeart.setImageResource(R.drawable.ic_full_heart);
+//        } else {
+//            // 좋아요를 누르지 않았으면 빈 하트 이미지 로드
+//            binding.icEmptyHeart.setImageResource(R.drawable.ic_empty_heart);
+//        }
+//    }
+//
+//    private void checkLikeStatus(String postId, String userId, ImageButton likeButton) {
+//        db.collection("posts").document(postId)
+//                .get()
+//                .addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        DocumentSnapshot document = task.getResult();
+//                        List<String> likes = (List<String>) document.get("likes");
+//                        if (likes != null && likes.contains(userId)) {
+//                            // 좋아요를 이미 눌렀으면 제거
+//                            removeLike(postId, userId, likeButton);
+//                        } else {
+//                            // 좋아요를 누르지 않았으면 추가
+//                            submitLike(postId, userId, likeButton);
+//                        }
+//                    } else {
+//                        // 에러 처리
+//                    }
+//                });
+//    }
+//
+//    private void submitLike(String postId, String userId, ImageButton likeButton) {
+//        db.collection("posts").document(postId)
+//                .update("likes", FieldValue.arrayUnion(userId))
+//                .addOnSuccessListener(aVoid -> {
+//                    // 좋아요 추가 성공
+//                    loadLikes(postId, userId, likeButton);
+//                })
+//                .addOnFailureListener(e -> {
+//                    // 에러 처리
+//                });
+//    }
+//
+//    private void removeLike(String postId, String userId, ImageButton likeButton) {
+//        db.collection("posts").document(postId)
+//                .update("likes", FieldValue.arrayRemove(userId))
+//                .addOnSuccessListener(aVoid -> {
+//                    // 좋아요 제거 성공
+//                    loadLikes(postId, userId, likeButton);
+//                })
+//                .addOnFailureListener(e -> {
+//                    // 에러 처리
+//                });
+//    }
+//
+//    private void loadLikes(String postId, String userId, ImageButton likeButton) {
+//        db.collection("posts").document(postId)
+//                .get()
+//                .addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        DocumentSnapshot document = task.getResult();
+//                        List<String> likes = (List<String>) document.get("likes");
+//                        int likesCount = likes != null ? likes.size() : 0;
+//                        // 좋아요 수에 따라 UI 업데이트
+//                        if (likes != null && likes.contains(userId)) {
+//                            // 좋아요를 이미 눌렀으면 꽉 찬 하트 이미지 로드
+//                            likeButton.setImageResource(R.drawable.ic_full_heart);
+//                        } else {
+//                            // 좋아요를 누르지 않았으면 빈 하트 이미지 로드
+//                            likeButton.setImageResource(R.drawable.ic_empty_heart);
+//                        }
+//                    } else {
+//                        // 에러 처리
+//                    }
+//                });
+//    }
 }
